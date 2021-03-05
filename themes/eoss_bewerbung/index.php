4249c91616ae70a5c77b9cd2f69ac57f25d45186
Daniel1971 <daniel.reisacher@bluewin.ch>
2021-03-05 06:55:57 +0100

    Update index.php
    HTML Tags auf Home/Blog

--- themes/eoss_bewerbung/index.php
+++ themes/eoss_bewerbung/index.php
@@ -19,18 +19,20 @@ get_header();
 
 		<?php
 		if ( have_posts() ) :
+		
+		echo ('<article>');
 
 			if ( is_home() && ! is_front_page() ) :
 				?>
-				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
 				<?php
 			endif;
 
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
+		
+			echo ('<section>');
 
 				/*
 				 * Include the Post-Type-specific template for the content.
@@ -38,15 +40,19 @@ get_header();
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
 				get_template_part( 'template-parts/content', get_post_type() );
+				
+			echo ('</section>');		
 
 			endwhile;
 
 			the_posts_navigation();
+		
+		echo ('</article>');
 
 		else :
-
+		
 			get_template_part( 'template-parts/content', 'none' );
-
+		
 		endif;
 		?>
 
